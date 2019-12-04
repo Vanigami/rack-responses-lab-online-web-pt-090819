0c06a8ac0e178f448c0142883162f8975968716f
Vanna Winland <vgwinland@gmail.com>
2019-12-04 21:37:28 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,5 +1,16 @@
 class Application
 
   def call(env)
-     resp = Response::application
-     
+     resp = Rack::Response.new
+
+     if Time.now.hour.between?(0,11)
+       resp.write "Good Morning!"
+     elsif Time.now.hour.between?(12,1)
+       resp.write "Good Afternoon"
+     else
+       resp.write "Good Evening!"
+     end
+
+     resp.finish
+   end
+ end
